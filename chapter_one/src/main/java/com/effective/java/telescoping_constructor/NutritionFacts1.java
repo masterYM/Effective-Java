0b96d9ec9fe69f1401a02e0d2c22445574a192b6
592306832@qq.com <592306832@qq.com>
2018-12-05 23:07:00 +0800

    Builder_pattern

--- chapter_one/src/main/java/com/effective/java/telescoping_constructor/NutritionFacts1.java
+++ chapter_one/src/main/java/com/effective/java/telescoping_constructor/NutritionFacts1.java
@@ -1,7 +1,7 @@
 package com.effective.java.telescoping_constructor;
 
 /**
- * 
+ * Builder模式
  */
 public class NutritionFacts1 {
     private final int servingSize;
@@ -10,4 +10,50 @@ public class NutritionFacts1 {
     private final int fat;
     private final int sodium;
     private final int carbohydrate;
+
+    public static class Builder{
+        //Required parameters
+        private final int servingSize;
+        private final int servings;
+
+        //Optional parameters - initialized to default values
+        private  int calories = 0;
+        private  int fat = 0;
+        private  int sodium = 0;
+        private  int carbohydrate = 0;
+
+        public Builder(int servingSize, int servings) {
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+        public Builder calories(int val){
+            calories = val;
+            return this;
+        }
+        public Builder fat(int val){
+            fat = val;
+            return this;
+        }
+        public Builder sodium(int val){
+            sodium = val;
+            return this;
+        }
+        public Builder carbohydrate(int val){
+            carbohydrate = val;
+            return this;
+        }
+        
+        public NutritionFacts1 build(){
+            return new NutritionFacts1(this);
+        }
+    }
+    private NutritionFacts1(Builder builder){
+        servingSize = builder.servingSize;
+        servings = builder.servings;
+        calories = builder.calories;
+        fat = builder.fat;
+        sodium = builder.sodium;
+        carbohydrate = builder.carbohydrate;
+    }
+
 }
