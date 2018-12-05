293ea42b6e2ad2719e39415666ea7b3aabd6aa2c
592306832@qq.com <592306832@qq.com>
2018-12-05 22:54:47 +0800

    Telescoping

--- chapter_one/src/main/java/com/effective/java/staticfactory/Services.java
+++ chapter_one/src/main/java/com/effective/java/staticfactory/Services.java
@@ -4,6 +4,10 @@ package com.effective.java.staticfactory;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * 服务提供者框架模式有着无数种变体
+ * 包含一个服务提供者接口和默认提供者 的简单实现
+ */
 //Noninstantiable class for service registration and access
 public class Services {
     private Services(){
@@ -14,6 +18,7 @@ public class Services {
 
     public static final String DEFAULT_PROVIDER_NAME = "<def>";
 
+    //Provider registration API
     public static void registerDefaultProvider(Provider p){
         registerProvider(DEFAULT_PROVIDER_NAME,p);
     }
@@ -22,6 +27,7 @@ public class Services {
         providers.put(name,p);
     }
 
+  //Service access API
     public static Service newInstance() throws IllegalAccessException {
         return newInstance(DEFAULT_PROVIDER_NAME);
     }
---
+++ chapter_one/src/main/java/com/effective/java/telescoping_constructor/NutritionFacts1.java
@@ -0,0 +1,13 @@
+package com.effective.java.telescoping_constructor;
+
+/**
+ * 
+ */
+public class NutritionFacts1 {
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrate;
+}
