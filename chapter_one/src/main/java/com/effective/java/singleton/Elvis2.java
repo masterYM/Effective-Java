3d17261dcb9d3a0d0760c63acacaffe8642be1c1
592306832@qq.com <592306832@qq.com>
2018-12-06 22:49:57 +0800

    -

--- chapter_one/src/main/java/com/effective/java/singleton/Elvis2.java
+++ chapter_one/src/main/java/com/effective/java/singleton/Elvis2.java
@@ -3,6 +3,10 @@ package com.effective.java.singleton;
 import java.io.Serializable;
 
 /**
+ * ###用私有构造器或者枚举类型强化Singleton属性
+ *
+ *
+ *
  * 为了使利用这其中一种方法实现的Singleton类变成是可序列化的，仅仅在声明中加上 implements Serializable 是不够的。
  * 为了维护并保证Singleton，必须声明所有实例域都是瞬时的，并提供一个readResolve方法，否则，每次反序列化一个序列化的实例时
  * 都会创建一个实例；
